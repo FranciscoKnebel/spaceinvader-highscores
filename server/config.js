3187f7ab1b77fb24f41dfb2954a7405e6d4ddf48
Francisco Knebel <franciscopaivaknebel@gmail.com>
2018-02-22 01:57:48 -0300

    Added cors.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const helmet = require('helmet');
 const morgan = require('morgan');
-
+const cors = require('cors');
 
 module.exports = (app) => {
 	app.use(helmet());
@@ -9,4 +9,26 @@ module.exports = (app) => {
 	app.engine('html', require('ejs').renderFile);
 
 	app.set('view engine', 'html');
+
+	const allowedOrigins = [
+		'http://localhost:8000',
+		'http://franciscoknebel.github.io',
+		'https://franciscoknebel.github.io'
+	];
+
+	app.use(cors({
+		origin(origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) {
+				return callback(null, true);
+			}
+
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg = `The CORS policy for this site does not allow access from the specified "${origin}" Origin.`;
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		}
+	}));
 };
